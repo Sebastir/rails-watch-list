1c8d0ec05b3ee8b9047c07d7ea01e5d17f68d265
Seb Rojas <src.py@icloud.com>
2022-10-27 19:58:11 +0200

    photo added

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,5 @@
 class Bookmark < ApplicationRecord
+  has_one_attached :photo
   belongs_to :movie
   belongs_to :list
   validates :movie_id, uniqueness: {scope: :list_id, message: 'is already in the list!' }
